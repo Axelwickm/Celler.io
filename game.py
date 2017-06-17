64922ea8ddc545807e9e45a6c07c07fb3cbb8b20
Axel Wickman <axelwickm@gmail.com>
2017-06-17 13:56:13 +0200

    Python file json input protocol

--- game.py
+++ game.py
@@ -5,9 +5,21 @@ from time import sleep
 def jprint(s):
 	print(json.dumps({'event':'print','data':s}));
 	sys.stdout.flush()
+	
+def onfullupdate(data):
+	pass
+
+def onpartialupdate(data):
+	pass
 
 
 jprint('testlog');
 while 1:
 	for line in sys.stdin:
-		jprint('gotdata')
+		parsed = json.loads(line);
+		if parsed.event == 'onfullupdate':
+			onfullupdate(parsed.data)
+		elif parsed.event == 'onpartialupdate':
+			onpartialupdate(parsed.data)
+		else:
+			jprint('unhandled data')
